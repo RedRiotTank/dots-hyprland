41f8f9228614053c47e6b13b10d85a15f09f5592
RedRiotTank <albertoplazamontesdm@gmail.com>
2026-09-05 19:50:57 +0200

    fix: es lang

--- dots/.config/hypr/hyprland/general.lua
+++ dots/.config/hypr/hyprland/general.lua
@@ -254,7 +254,7 @@ hl.animation({
 
 hl.config({
     input = {
-        kb_layout = "us",
+        kb_layout = "es",
         numlock_by_default = true,
         repeat_delay = 250,
         repeat_rate = 35,
